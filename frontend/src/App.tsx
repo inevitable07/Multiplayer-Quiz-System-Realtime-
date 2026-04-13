f6e79d68ea25d721283258c0e822e36a430dbb12
Aashish Bhaskar <imaashishbhaskar@gmail.com>
2026-04-14 01:47:24 +0530

    refactor: migrate codebase from javascript to typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { useState, FC } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: FC = () => {
+  const [count, setCount] = useState<number>(0)
 
   return (
     <>
@@ -18,12 +18,12 @@ function App() {
         <div>
           <h1>Get started</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
         <button
           className="counter"
-          onClick={() => setCount((count) => count + 1)}
+          onClick={() => setCount((count: number) => count + 1)}
         >
           Count is {count}
         </button>
